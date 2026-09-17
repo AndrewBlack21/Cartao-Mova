c5c9efc7711425a70e0d6f243ffecb369ddb36a5
Andrew Ferreira <161402940+AndrewBlack21@users.noreply.github.com>
2026-09-17 13:33:15 -0300

    Update card information for social media links
    Updated Instagram details and removed email, LinkedIn, and website fields.

--- src/data/card.js
+++ src/data/card.js
@@ -1,16 +1,15 @@
 export const card = {
   company: "CAN GROUP",
   slogan: "Conectando pessoas e negócios.",
-  instagram: "https://www.instagram.com/cangroupbr/",
-  instagramLabel: "@cangroupbr",
+  instagram: "https://www.instagram.com/movamaker/",
+  instagramLabel: "@movamaker",
   whatsappNumber: "5513999999999", // troque pelo número oficial com DDI
   whatsappDisplay: "(13) 99999-9999",
-  email: "can@cangroup.com.br",
+  
   phone: "+55 13 99999-9999",
   address: "Santos - SP",
   mapsUrl: "https://www.google.com/maps/search/?api=1&query=Santos%20SP",
-  linkedin: "https://www.linkedin.com/",
-  website: "https://cangroup.com.br",
+
 };
 
 export function whatsappUrl(message = "Olá! Gostaria de falar com a CAN GROUP.") {
